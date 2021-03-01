e2684a6b9cc7079d2fba3570dbfc78f274815b96
Tester <49004622+Pawi2323@users.noreply.github.com>
2021-03-01 09:25:06 +0100

    Wyświetlanie tablicy dwuwymiarowej

--- Tablica Dwuwymiarowa/TablicaDwuwymiarowa.java
+++ Tablica Dwuwymiarowa/TablicaDwuwymiarowa.java
@@ -4,10 +4,16 @@ class TablicaDwuwymiarowa{
 	public static void main(String[] args){
 		int [] tab = new int[5];
 		tab[0] = 40;
-		for(int i=0; i<tab.length; i++){
-			System.out.println(tab[i]);
-		}
-		
 		int [][] tab2d = new int [5][2];
+		tab2d[0][1] = 50;
+		tab2d[2][1] = -200;
+		tab2d[4][0] = 444;
+		for (int i=0; i<tab2d.length; i++){
+			for (int j=0; j<tab2d[i].length; j++){
+				System.out.println(tab2d[i][j]);
+			}
+			System.out.println("Koniec petli ze zmienna j");
+		}
+		System.out.println("Koniec petli ze zmienna i");
 	}
 }
