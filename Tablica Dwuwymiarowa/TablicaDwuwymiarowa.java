6e8a55fc33c65c11131c56b81f6fc4cecdace5f9
Tester <49004622+Pawi2323@users.noreply.github.com>
2021-03-01 09:11:02 +0100

    Stworzenie tablicy dwuwymiarowej

--- Tablica Dwuwymiarowa/TablicaDwuwymiarowa.java
+++ Tablica Dwuwymiarowa/TablicaDwuwymiarowa.java
@@ -4,6 +4,10 @@ class TablicaDwuwymiarowa{
 	public static void main(String[] args){
 		int [] tab = new int[5];
 		tab[0] = 40;
-		tab[1] = "Jeden" //error
+		for(int i=0; i<tab.length; i++){
+			System.out.println(tab[i]);
+		}
+		
+		int [][] tab2d = new int [5][2];
 	}
 }
