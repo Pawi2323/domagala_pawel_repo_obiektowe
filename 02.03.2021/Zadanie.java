59a769c04c8c776e0c62766e414bfa3a57378cd9
Tester <49004622+Pawi2323@users.noreply.github.com>
2021-03-02 10:08:26 +0100

    Zadania z tablicą

---
+++ 02.03.2021/Zadanie.java
@@ -0,0 +1,20 @@
+
+
+public class Zadanie{
+	public static void main(String[] args){
+		tablica();
+	}
+	private static void tablica(){
+		int [][] tab = new int [2][2];
+		tab [0][0] = 1;
+		tab [0][1] = 2;
+		tab [1][0] = 3;
+		tab [1][1] = 4;
+		
+		for (int i=0; i<tab.length; i++){
+			for (int j=0; j<tab[i].length; j++){
+				System.out.println(tab[i][j]);
+			}
+		}
+	}
+}
