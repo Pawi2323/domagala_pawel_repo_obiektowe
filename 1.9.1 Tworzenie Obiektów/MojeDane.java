65560687329d69147d654c19253d8d341ba11334
Pawcio <49004622+Pawi2323@users.noreply.github.com>
2021-03-30 09:09:40 +0200

    plik do zadania 1

---
+++ 1.9.1 Tworzenie Obiektów/MojeDane.java
@@ -0,0 +1,12 @@
+class MojeDane{
+	public String imie = "imie";
+	public String nazwisko = "nazwisko";
+	public String klasa = "klasa";
+
+	public MojeDane(String imie, String nazwisko, String klasa){
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+		this.klasa = klasa;
+	}
+}
+
