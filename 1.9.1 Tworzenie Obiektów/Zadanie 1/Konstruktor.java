9bd56d56984611ceb2528c266198fcf1e5ed1d1f
Pawcio <49004622+Pawi2323@users.noreply.github.com>
2021-03-30 10:29:58 +0200

    Delete 1.9.1 Tworzenie Obiektów/Zadanie 1 directory

--- 1.9.1 Tworzenie Obiektów/Zadanie 1/Konstruktor.java
+++
@@ -1,6 +0,0 @@
-class Konstruktor{
-	public static void main (String[] args){
-		MojeDane md = new MojeDane("Imie", "Nazwisko", "Klasa");
-		System.out.println(md.imie+" "+md.nazwisko+" "+md.klasa);
-	}
-}
