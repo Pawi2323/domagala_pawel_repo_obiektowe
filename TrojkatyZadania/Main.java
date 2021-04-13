0054ee5aa61873fbdda48b26bc270c4a944174a1
Pawcio <49004622+Pawi2323@users.noreply.github.com>
2021-04-13 10:13:06 +0200

    commit 2

--- TrojkatyZadania/Main.java
+++ TrojkatyZadania/Main.java
@@ -1,7 +1,7 @@
 
 class Main{
 	public static void main(String[] args){
-		Trojkat t1 = new Trojkat(3,0,0,4);
-		System.out.println(t1);
+		Trojkat t1 = new Trojkat(3,4);
+		Trojkat t2 = new Trojkat(3);
 	}
 }
--- TrojkatyZadania/Trojkat.java
+++ TrojkatyZadania/Trojkat.java
@@ -1,19 +1,10 @@
 
 class Trojkat{
-	private int a = 0;
-	private int b = 0;
-	private int c = 0;
-	private int h = 0;
-	
-	public Trojkat(int a,int b,int c,int h ){
-		this.a = a;
-		this.b = b;
-		this.c = c;
-		this.h = h;
+
+	public Trojkat(double a,double h ){
+		System.out.println("Pole trojkata prostokata: "+(a*h/2));
 	}
-	
-	@Override
-	public String toString(){
-		return "Pole trojkata: "+(a*h/2);
+	public Trojkat(double a){
+		System.out.println("Pole trojkata prostokata: "+((a*a)*Math.pow(a, 1/3))/4);
 	}
 }
