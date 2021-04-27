0447842e921ab859d99d7504b934330a0ac2dcbc
Pawel <49004622+Pawi2323@users.noreply.github.com>
2021-04-27 10:24:28 +0200

    commit

---
+++ ToDo/ToDo.java
@@ -0,0 +1,54 @@
+import java.time.LocalDateTime;
+
+class ToDo {
+	private String title = "";
+	private String content = "";
+	private String author = "Anonymous";
+	private LocalDateTime add_date = LocalDateTime.now();
+	
+	//1 Constructor author unknown
+	public ToDo(String title, String content){
+		this.title = title;
+		this.content = content;
+	}
+	
+	//2 Constructor 
+	public ToDo(String title, String content, String author){
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		add_date = LocalDateTime.now();
+	}
+	
+	//3 Constructor 
+	public ToDo(String title, String content, String author, LocalDateTime add_date){
+		this.title = title;
+		this.content = content;
+		this.author = author;
+		this.add_date = add_date;
+	}
+	
+	public String getContent(){
+		return content;
+	}
+	
+	public LocalDateTime getAdd_Date(){
+		return add_date;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	@Override
+	public String toString(){
+		return "Title: "+title+"\n"
+			+"Content: "+content+"\n"
+			+"Author: "+author+"\n"
+			+"Data: "+add_date+"\n";
+	}
+}
